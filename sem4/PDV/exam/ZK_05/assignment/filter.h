d99db9acbaa425022a166425e3393f81f8cf5eb6
kolesole <kolesole@fel.cvut.cz>
2025-07-13 20:59:37 +0200

    .

--- sem4/PDV/exam/ZK_05/assignment/filter.h
+++
@@ -1,9 +0,0 @@
-#ifndef ZK_05_FILTER_H
-#define ZK_05_FILTER_H
-
-#include "data_table.h"
-
-void filtering(data_table& table, unsigned int iterations);
-void filtering_par(data_table& table, unsigned int iterations);
-
-#endif //ZK_05_FILTER_H
